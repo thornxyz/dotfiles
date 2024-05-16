dc0f9ab5814dfe31e9f16f765d684b431049a848
thornxyz <subhrajyotighosh972@gmail.com>
2024-05-16 20:32:19 +0530

    changed some

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,8 +2,4 @@ require "nvchad.options"
 
 -- add yours here!
 
-vim.o.shortmess = vim.o.shortmess:gsub('I', '')
-
-local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
-
+vim.o.shortmess = vim.o.shortmess:gsub("I", "")
